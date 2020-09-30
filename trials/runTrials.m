d9b2c68d36d27ac7c6125ef899310b1f8afd186e
Martin <martin@MacBook-Air-de-Martin.local>
2020-09-30 17:52:47 +0200

    change to see first trigger in bull's eye

--- trials/runTrials.m
+++ trials/runTrials.m
@@ -36,7 +36,7 @@ for t = 1:const.blk_step
     if t == 1
         % show the iti image
         Screen('FillRect',scr.main,const.background_color);
-        drawTarget(scr,const,scr.x_mid,scr.y_mid);
+        drawEmptyTarget(scr,const,scr.x_mid,scr.y_mid);
         Screen('Flip',scr.main);
         
         first_trigger           =   0;
