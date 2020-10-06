f0039b853250b3e9b04304f163e65c0eccb5ce39
Martin Szinte <martinszinte@macbookpro-martin.local>
2020-10-06 16:37:51 +0200

    changes

--- config/dirSaveFile.m
+++ config/dirSaveFile.m
@@ -12,30 +12,30 @@ function [const] = dirSaveFile(const)
 % const : struct containing constant configurations
 % ----------------------------------------------------------------------
 % Function created by Martin SZINTE (martin.szinte@gmail.com)
-% Last update : 20 / 06 / 2020
+% Last update : 06 / 10 / 2020
 % Project :     RSexp
 % Version :     1.0
 % ----------------------------------------------------------------------
 
 % Create data directory 
-if ~isdir(sprintf('data/%s/func/',const.sjct))
-    mkdir(sprintf('data/%s/func/',const.sjct))
+if ~isdir(sprintf('data/%s/%s/func/',const.sjct,const.session))
+    mkdir(sprintf('data/%s/%s/func/',const.sjct,const.session))
 end
 
 % Define directory
-const.dat_output_file   =   sprintf('data/%s/func/%s_task-%s_%s',const.sjct,const.sjct,const.cond1_txt,const.run_txt);
+const.dat_output_file   =   sprintf('data/%s/%s/func/%s_%s_task-%s_%s',const.sjct,const.session,const.sjct,const.session,const.cond1_txt,const.run_txt);
 
 % Eye data
 const.eyelink_temp_file =   'XX.edf';
 const.eyelink_local_file=   sprintf('%s_eyeData.edf',const.dat_output_file);
 
 % Create additional info directory
-if ~isdir(sprintf('data/%s/add/',const.sjct))
-    mkdir(sprintf('data/%s/add/',const.sjct))
+if ~isdir(sprintf('data/%s/%s/add/',const.sjct,const.session))
+    mkdir(sprintf('data/%s/%s/add/',const.sjct,const.session))
 end
 
 % Define directory
-const.add_output_file   =   sprintf('data/%s/add/%s_task-%s_%s',const.sjct,const.sjct,const.cond1_txt,const.run_txt);
+const.add_output_file   =   sprintf('data/%s/%s/add/%s_%s_task-%s_%s',const.sjct,const.session,const.sjct,const.session,const.cond1_txt,const.run_txt);
 
 % Define .mat saving file
 const.mat_file          =   sprintf('%s_matFile.mat',const.add_output_file);
